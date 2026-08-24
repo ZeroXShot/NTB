1ab4a31ec586fd44ff69ca0c6d90ad475d415346
ZeroXShot <j11rootx@gmail.com>
2026-08-24 14:29:39 +0200

    feat: add the ntb package root

---
+++ src/ntb/__init__.py
@@ -0,0 +1,9 @@
+"""NTB - Neural Tensor Builder.
+
+Build AI architectures graphically in 2D and 3D, then emit them to PyTorch,
+Keras 3 or ONNX.
+"""
+
+__version__ = "0.0.1.dev0"
+
+__all__ = ["__version__"]
